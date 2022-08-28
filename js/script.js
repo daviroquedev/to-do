1314a1a5bbffd81c59cec9d677fe5dfa2665c9df
Davi Roque Maia de Sousa <sowdavi@gmail.com>
2022-08-28 01:04:39 -0300

    function filter to-do //em att

--- js/script.js
+++ js/script.js
@@ -4,8 +4,9 @@ const todoForm = document.querySelector("#todo-form")
 const todoInput = document.querySelector("#todo-input")
 const todoList = document.querySelector("#todo-list")
 const editForm = document.querySelector("#edit-form")
-const editInput= document.querySelector("#edit-input")
+const editInput = document.querySelector("#edit-input")
 const cancelEditBtn = document.querySelector("#cancel-edit-btn")
+
 
 let oldInputValue;
 
@@ -18,7 +19,7 @@ const saveTodo = (text) => {
     const todoTitle = document.createElement("h3")
     todoTitle.innerText = text
     todo.appendChild(todoTitle)
-    
+
     const doneBtn = document.createElement("button")
     doneBtn.classList.add("finish-todo")
     doneBtn.innerHTML = '<i class="fa-solid fa-check"></i>'
@@ -50,12 +51,14 @@ const updateTodo = (text) => {
 
     const todos = document.querySelectorAll(".todo")
 
-    todos.forEach((todo)=>{
+    todos.forEach((todo) => {
 
-        let todoTitle= todo.querySelector("h3")
+        let todoTitle = todo.querySelector("h3")
 
-        if(todoTitle.innerText === oldInputValue){
+        if (todoTitle.innerText === oldInputValue) {
             todoTitle.innerText = text
+
+
         }
     })
 
@@ -65,10 +68,10 @@ const updateTodo = (text) => {
 
 todoForm.addEventListener("submit", (e) => {
     e.preventDefault();
-    
+
     const inputValue = todoInput.value
 
-    if(inputValue){
+    if (inputValue) {
         // save todo
         saveTodo(inputValue)
     }
@@ -80,41 +83,122 @@ document.addEventListener("click", (e) => {
     const parentEl = targetEl.closest("div");
     let todoTitle;
 
-    if(parentEl && parentEl.querySelector("h3")){
+    if (parentEl && parentEl.querySelector("h3")) {
         todoTitle = parentEl.querySelector("h3").innerText;
     }
 
-    if(targetEl.classList.contains("finish-todo")){
+    if (targetEl.classList.contains("finish-todo")) {
         parentEl.classList.toggle("done")
     }
 
-    if(targetEl.classList.contains("remove-todo")){
+    if (targetEl.classList.contains("remove-todo")) {
         parentEl.remove()
     }
 
-    if(targetEl.classList.contains("edit-todo")){
-       toggleForms()
+    if (targetEl.classList.contains("edit-todo")) {
+        toggleForms()
 
-       editInput.value = todoTitle;
-       oldInputValue = todoTitle; 
+        editInput.value = todoTitle;
+        oldInputValue = todoTitle;
     }
 })
 
-cancelEditBtn.addEventListener("click",(e) => {
+cancelEditBtn.addEventListener("click", (e) => {
     e.preventDefault();
 
     toggleForms();
 })
 
 editForm.addEventListener("submit", (e) => {
+
     e.preventDefault()
-    
+
     const editInputValue = editInput.value
 
-    if(editInputValue){
+    if (editInputValue) {
         // atualizar
         updateTodo(editInputValue)
     }
 
     toggleForms()
 })
+
+// FILTRAR LISTAS
+
+
+const filterSelect = document.getElementById("filter-select");
+
+function onChange() {
+
+    // capturei os valores dos filtros
+    const valueFilter = filterSelect.value;
+    const textFilter = filterSelect.options[filterSelect.selectedIndex].text;
+
+    const todoClass = document.querySelectorAll("#todo-list > *")
+
+    console.log(valueFilter, textFilter);
+
+
+
+    if (valueFilter == "all") {
+        // exibe todos
+        // remove todas as classes hide
+
+        todoClass.forEach((item) => {
+            const itemList = item
+            console.log(itemList)
+        
+            if(itemList.classList.contains("hide")){
+                itemList.classList.remove("hide")
+                itemList.classList.add("todo")
+                
+            }
+             
+        })
+
+    }
+
+    if (valueFilter == "done") {
+        // se nao tem a classe done, fica "hide"
+        //so mostra os done..
+        //done? exibe : coloca classe hide
+        
+        todoClass.forEach((item) => {
+            const itemList = item
+            console.log(itemList)
+        
+            if(itemList.classList.contains("done")){
+                itemList.classList.remove("hide")
+                itemList.classList.add("todo")
+               
+            } else {
+                itemList.classList.add("hide")
+                itemList.classList.remove("todo")
+            }
+             
+        })
+
+    }
+    if (valueFilter == "todo") {
+        // exibe todos que nao tem a classe Done
+        // se tiver classe done fica "hide"
+        //hide em todos que tem a classe done <<
+        todoClass.forEach((item) => {
+            const itemList = item
+            console.log(itemList)
+
+            if(itemList.classList.contains("done")){
+                itemList.classList.add("hide")
+                itemList.classList.remove("todo")
+                itemList.classList.remove('done')
+            }
+         
+
+        })
+
+
+    }
+}
+
+filterSelect.onchange = onChange;
+onChange();
